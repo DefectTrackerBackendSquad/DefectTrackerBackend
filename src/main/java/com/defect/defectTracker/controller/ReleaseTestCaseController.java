b04caa5acffc176594a6803db151773f5c479cc3
thivi <thivi_email@example.com>
2025-06-21 17:48:22 +0530

    Changes on controller

--- src/main/java/com/defect/defectTracker/controller/ReleaseTestCaseController.java
+++ src/main/java/com/defect/defectTracker/controller/ReleaseTestCaseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/releases")
+@RequestMapping("/api/v1/releaseTestCase")
 @RequiredArgsConstructor
 
 
