bf25a7848779ec360392422412fb82d1a0c98774
jdilux <dilux1022@gmail.com>
2025-06-21 16:14:03 +0530

    Get Defect By Testcase Id

--- src/main/java/com/defect/defectTracker/dto/DefectDto.java
+++ src/main/java/com/defect/defectTracker/dto/DefectDto.java
@@ -1,4 +1,19 @@
 package com.defect.defectTracker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DefectDto {
+    private String id;
+    private String defectTitle;
+    private String descriptions;
+    private String testcaseId;
+    private String severity;
+    private String defectStatus;
 }
