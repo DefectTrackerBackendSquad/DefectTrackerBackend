80e603c330cd4dc44203063d9727c7153d382962
JANANAN26 <ujananan26@gmail.com>
2025-06-22 19:48:24 +0530

    Testcase export

--- src/main/java/com/defect/defectTracker/service/ReleaseTestCaseService.java
+++
@@ -1,7 +0,0 @@
-package com.defect.defectTracker.service;
-
-import com.defect.defectTracker.dto.TestCaseResponseDTO;
-
-public interface ReleaseTestCaseService {
-    TestCaseResponseDTO getTestCaseByReleaseTestCaseId(String releaseTestCaseId);
-}
