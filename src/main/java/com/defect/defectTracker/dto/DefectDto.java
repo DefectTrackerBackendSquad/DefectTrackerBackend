b101f779797a6c74a50b2fb34bfcfa03da099723
Abisherk2023 <146057097+Abisherk2023@users.noreply.github.com>
2025-06-22 11:48:35 +0530

    Revert "Get defect by testCase Id"

--- src/main/java/com/defect/defectTracker/dto/DefectDto.java
+++ src/main/java/com/defect/defectTracker/dto/DefectDto.java
@@ -1,15 +1,4 @@
 package com.defect.defectTracker.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class DefectDto {
-    private String id;
-    private String defectTitle;
-    private String descriptions;
-    private String testcaseId;
-    private String severity;
-    private String defectStatus;
 }
--- src/main/java/com/defect/defectTracker/repository/DefectRepo.java
+++ src/main/java/com/defect/defectTracker/repository/DefectRepo.java
@@ -2,12 +2,10 @@ package com.defect.defectTracker.repository;
 
 import com.defect.defectTracker.entity.Defect;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface DefectRepo extends JpaRepository<Defect, Long> {
-    @Query("SELECT d FROM Defect d WHERE d.releaseTestCase.testcaseId = :testcaseId")
-    Optional<Defect> findByTestcaseId(@Param("testcaseId") String testcaseId);
+@Repository
+public interface DefectRepo extends JpaRepository<Defect, String> {
 }
