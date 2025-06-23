6900dd6232b0fd6b02275fbda89886f382c7a434
Ithayaraj <ithayarajim15@gmail.com>
2025-06-23 14:26:41 +0530

    Release Apis

--- src/main/java/com/defect/defectTracker/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/defect/defectTracker/exceptionHandler/GlobalExceptionHandler.java
@@ -1,7 +1,35 @@
 package com.defect.defectTracker.exceptionHandler;
 
+import com.defect.defectTracker.utils.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class GlobalExceptionHandler extends RuntimeException {
     public GlobalExceptionHandler(String message) {
         super(message);
     }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Constants.responseBuilder(
+                        HttpStatus.NOT_FOUND.value(),
+                        Constants.STATUS_FAILURE,
+                        Constants.DATA_NOT_FOUND,
+                        ex.getMessage(),
+                        null));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        return ResponseEntity.badRequest()
+                .body(Constants.responseBuilder(
+                        HttpStatus.BAD_REQUEST.value(),
+                        Constants.STATUS_FAILURE,
+                        Constants.RETRIEVE_FAILED,
+                        ex.getMessage(),
+                        null));
+    }
+
+
 }
