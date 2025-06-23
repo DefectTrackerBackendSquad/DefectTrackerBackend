8078955db42ebefc165b2715c247ba1b58b1374f
ragupalan-r <ragupalant@gmail.com>
2025-06-23 10:20:18 +0530

    Update DefectStatusRepo.java

--- src/main/java/com/defect/defectTracker/repository/DefectStatusRepo.java
+++ src/main/java/com/defect/defectTracker/repository/DefectStatusRepo.java
@@ -4,7 +4,7 @@ import com.defect.defectTracker.entity.DefectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+@Repository
 public interface DefectStatusRepo extends JpaRepository<DefectStatus, Long> {
     Optional<DefectStatus> findByDefectStatusNameIgnoreCase(String defectStatusName);
 }
