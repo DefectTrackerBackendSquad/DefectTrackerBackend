e98a0d419593a3d9b876437d40c7467737474caa
Abiram Pathmanathan <abirammaha22@icloud.com>
2025-06-23 14:54:35 +0530

    Revert "Defect status manoj"

--- src/main/java/com/defect/defectTracker/service/DefectStatusService.java
+++ src/main/java/com/defect/defectTracker/service/DefectStatusService.java
@@ -1,12 +1,8 @@
 package com.defect.defectTracker.service;
 
-import com.defect.defectTracker.dto.DefectStatusDTO;
 
 import com.defect.defectTracker.utils.StandardResponse;
 
 public interface DefectStatusService {
-    DefectStatusDTO createDefectStatus(DefectStatusDTO defectStatusDTO);
     StandardResponse deleteDefectStatus(Long defectStatusId);
 }
-
-
