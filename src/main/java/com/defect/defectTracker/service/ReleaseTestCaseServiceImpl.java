395102ceb692757d96eb3ed0f77f54b44fb81bdc
JANANAN26 <ujananan26@gmail.com>
2025-06-23 11:13:48 +0530

    Release Testcase Get, Delete

--- src/main/java/com/defect/defectTracker/service/ReleaseTestCaseServiceImpl.java
+++ src/main/java/com/defect/defectTracker/service/ReleaseTestCaseServiceImpl.java
@@ -2,15 +2,11 @@ package com.defect.defectTracker.service;
 
 import com.defect.defectTracker.dto.TestCaseResponseDTO;
 import com.defect.defectTracker.entity.ReleaseTestCase;
-import com.defect.defectTracker.repository.ReleaseTestCaseRepository;
 import com.defect.defectTracker.exceptionHandler.ResourceNotFoundException;
 import com.defect.defectTracker.repository.ReleaseTestCaseRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @Transactional
@@ -22,7 +18,7 @@ public class ReleaseTestCaseServiceImpl implements ReleaseTestCaseService {
     @Override
     public TestCaseResponseDTO getTestCaseByReleaseTestCaseId(String releaseTestCaseId) {
         ReleaseTestCase rtc = releaseTestCaseRepo.findByReleaseTestCaseId(releaseTestCaseId)
-                .orElseThrow(() -> new NoSuchElementException("Data not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Data not found"));
 
         TestCaseResponseDTO dto = new TestCaseResponseDTO();
         dto.setReleasedTestcaseId(rtc.getReleaseTestCaseId());
@@ -40,5 +36,14 @@ public class ReleaseTestCaseServiceImpl implements ReleaseTestCaseService {
         dto.setTestcaseStatus("Passed".equalsIgnoreCase(rtc.getTestCaseStatus()) ? 1 : 0);
 
         return dto;
+    }
+
+    @Override
+    public void deleteTestCaseByReleaseTestCaseId(String releaseTestCaseId) {
+        boolean exists = releaseTestCaseRepo.existsByReleaseTestCaseId(releaseTestCaseId);
+        if (!exists) {
+            throw new ResourceNotFoundException("ReleaseTestCase with ID " + releaseTestCaseId + " not found");
+        }
+        releaseTestCaseRepo.deleteByReleaseTestCaseId(releaseTestCaseId);
     }
 }
