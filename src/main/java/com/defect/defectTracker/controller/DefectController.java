a319bd0c7ea94bb66bab3f00876233f91549c3c2
jdilux <dilux1022@gmail.com>
2025-06-24 15:20:37 +0530

    Updating Defect Get

--- src/main/java/com/defect/defectTracker/controller/DefectController.java
+++ src/main/java/com/defect/defectTracker/controller/DefectController.java
@@ -1,17 +1,39 @@
 package com.defect.defectTracker.controller;
 
 import com.defect.defectTracker.dto.DefectDto;
+import com.defect.defectTracker.exceptionHandler.GlobalExceptionHandler;
 import com.defect.defectTracker.service.DefectService;
-import com.defect.defectTracker.utils.StandardResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/defect")
-@RequiredArgsConstructor
+@RequestMapping("/api/defects")
+@AllArgsConstructor
 public class DefectController {
 
+    private final DefectService defectService;
+
+    @GetMapping("/testcase/{testcaseId}")
+    public ResponseEntity<ApiResponse> getDefectByTestcaseId(@PathVariable String testcaseId) {
+        try {
+            DefectDto dto = defectService.getDefectByTestcaseId(testcaseId);
+            return ResponseEntity.ok(new ApiResponse(20000, "Retrieved Successfully", dto));
+        } catch (GlobalExceptionHandler ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse(40000, ex.getMessage(), null));
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    static class ApiResponse {
+        private int statusCode;
+        private String message;
+        private Object data;
+    }
 }
